8915f65a00c35f048c4d3352047f136c419ac0df
David Vradenburg <davidv@innosphere.ca>
2014-01-13 14:22:11 -0500

    Initial Commit

---
+++ ForceOrientationChange/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ForceOrientationChange
+//
+//  Created by David Vradenburg on 2014-01-13.
+//  Copyright (c) 2014 rhender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
